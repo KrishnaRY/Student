7426dc40ce7d53f59db8fff2f1d94ffe7f590dff
Reddy <661225@vnameits-Mac-mini.local>
2017-11-17 19:42:14 +0530

    test

---
+++ src/1.sql
@@ -0,0 +1,7 @@
+CREATE  TABLE IF NOT EXISTS `student` (
+  `userName` VARCHAR(255) ,
+  `firstName` VARCHAR(255) ,
+  `lastName` VARCHAR(255) ,
+   `password` VARCHAR(255) ,
+   `emailAddress` VARCHAR(255) ,
+  `dateOfBirth` DATE);
